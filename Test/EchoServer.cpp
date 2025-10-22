03090d64f23d308201127d9eb130d0b427652f92
nianzhibai <nianzhibai@gmail.com>
2025-10-22 20:13:52 +0800

    删除了Test目录下的一些文件

--- Test/EchoServer.cpp
+++
@@ -1,7 +0,0 @@
-#include "../CloudStorage/TcpServer/Acceptor.hpp"
-
-int main()
-{
-    Acceptor acceptor(5);
-    return 0;
-}
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,25 +1,14 @@
 #include "../CloudStorage/Log/Log.hpp"
-#include "../CloudStorage/TcpServer/SocketBuffer.hpp"
-
+#include <iostream>
+#include <sys/stat.h>
 
 int main()
 {
-    // Buffer buf;
-    // std::string str = "hello world";
-    // buf.WriteInBuffer(str.size(), str.c_str());
-    // std::string text = buf.ReadFromBuffer();
-    // std::cout << text << std::endl;
-
-    Buffer buf;
-    for(int i = 0; i <= 1029; i++)
+    struct stat statbuf; 
+    if(stat("./test.txt", &statbuf) == -1)
     {
-        buf.WriteInBuffer(1, "w");
+        std::cout << EACCES << std::endl;
+        std::cout << errno << std::endl;
     }
-
-    std::cout << buf.WriteAbleSize() << std::endl;
-
-    std::cout << buf.ReadAbleSize() << std::endl;
-    buf.ReadFromBuffer();
-    std::cout << buf.ReadAbleSize() << std::endl;
     return 0;
 }
