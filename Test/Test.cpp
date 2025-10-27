8cae1c79d43443af9bfa6972d1d9a6190b03bcfd
nianzhibai <nianzhibai@gmail.com>
2025-10-27 20:15:21 +0800

    修改了Test目录文件

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,36 +1,26 @@
-#include "../CloudStorage/Log/Log.hpp"
+#include "../CloudStorage/TcpServer/Buffer.hpp"
 #include <iostream>
-#include <sys/stat.h>
-#include <fstream>
-#include "../CloudStorage/TcpServer/Util.hpp"
 
 int main()
 {
-    // std::ofstream ofs("test.txt", std::ios_base::binary);
-    // ofs.seekp(10);
-    // if (ofs.rdstate() == std::ios_base::failbit)
-    // {
-    //     std::cout << "移动文件指针失败" << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
-    // ofs.write("hello", 5);
-    // if (ofs.rdstate() == std::ios_base::failbit)
-    // {
-    //     std::cout << "写入文件失败" << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
-    // std::string pathname = "./Hello";
-    // if(mkdir(pathname.c_str(), 0777) == -1)
-    // {
-    //     std::cout << "创建目录失败:" << strerror(errno) << std::endl;
-    //     exit(EXIT_FAILURE);
-    // }
+    // Buffer buf;
+    // std::string tmp = "hello world";
+    // std::cout << buf.ReadAbleSize() << std::endl;
+    // buf.WriteInBuffer(tmp.size(), tmp.c_str());
+    // std::cout << buf.ReadAbleSize() << std::endl;
+    // buf.ReadAllFromBuffer();
+    // std::cout << buf.ReadAbleSize() << std::endl;
 
-    // // 目录mkdir创建不能嵌套
-    // std::string filename = "./Hello/helloworld.txt";
-    // FileNameOk(filename);
-    std::string filename = "./You/And/Me/youandme.txt";
-    FileUtil::FileNameOk(filename);
-
+    Buffer buf;
+    std::cout << buf.Capacity() << std::endl;
+    for(int i = 0; i < 1025; i++)
+    {
+        buf.WriteInBuffer(1, "h");
+    }
+    std::cout << buf.ReadAbleSize() << std::endl;
+    std::cout << buf.Capacity() << std::endl;
+    buf.ReadAllFromBuffer();
+    std::cout << buf.ReadAbleSize() << std::endl;
+    std::cout << buf.Capacity() << std::endl;
     return 0;
 }
